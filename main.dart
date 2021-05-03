4f1742c241f056982afb56d055432bc954983520
tikasmiftahuljannah <79125600+tikasmiftahuljannah@users.noreply.github.com>
2021-05-04 04:51:16 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+import 'databaseHelper.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PerpustakaanOnline',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final DbStudentManager dbmanager = new DbStudentManager();
+
+  final _nameController = TextEditingController();
+  final _kelasController = TextEditingController();
+  final _formKey = new GlobalKey<FormState>();
+  Student student;
+  List<Student> studlist;
+  int updateIndex;
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Perpustakaan'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: new InputDecoration(labelText: 'Nama buku'),
+                    controller: _nameController,
+                    validator: (val) =>
+                        val.isNotEmpty ? null : 'Name Should Not Be empty',
+                  ),
+                  TextFormField(
+                    decoration: new InputDecoration(labelText: 'Lama Pinjam'),
+                    controller: _kelasController,
+                    validator: (val) =>
+                        val.isNotEmpty ? null : 'Kelas Should Not Be empty',
+                  ),
+                  TextFormField(
+                    decoration: new InputDecoration(labelText: 'Keterangan'),
+                    controller: _kelasController,
+                    validator: (val) =>
+                        val.isNotEmpty ? null : 'Alamat Should Not Be empty',
+                  ),
+                  // ignore: deprecated_member_use
+                  FlatButton(
+                    textColor: Colors.white,
+                    color: Colors.blueAccent,
+                    child: Container(
+                        width: width * 0.9,
+                        child: Text(
+                          'Submit',
+                          textAlign: TextAlign.center,
+                        )),
+                    onPressed: () {
+                      _submitStudent(context);
+                    },
+                  ),
+                  FutureBuilder(
+                    future: dbmanager.getStudentList(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        studlist = snapshot.data;
+                        return ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: studlist == null ? 0 : studlist.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            Student st = studlist[index];
+                            return Card(
+                              child: Row(
+                                children: <Widget>[
+                                  Container(
+                                    width: width * 0.6,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Text(
+                                          'Name: ${st.name}',
+                                          style: TextStyle(fontSize: 15),
+                                        ),
+                                        Text(
+                                          'Course: ${st.course}',
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              color: Colors.black54),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      _nameController.text = st.name;
+                                      _kelasController.text = st.course;
+                                      student = st;
+                                      updateIndex = index;
+                                    },
+                                    icon: Icon(
+                                      Icons.edit,
+                                      color: Colors.blueAccent,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      dbmanager.deleteStudent(st.id);
+                                      setState(() {
+                                        studlist.removeAt(index);
+                                      });
+                                    },
+                                    icon: Icon(
+                                      Icons.delete,
+                                      color: Colors.red,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            );
+                          },
+                        );
+                      }
+                      return new CircularProgressIndicator();
+                    },
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _submitStudent(BuildContext context) {
+    if (_formKey.currentState.validate()) {
+      if (student == null) {
+        Student st = new Student(
+            name: _nameController.text, course: _kelasController.text);
+        dbmanager.insertStudent(st).then((id) => {
+              _nameController.clear(),
+              _kelasController.clear(),
+              print('Student Added to Db $id')
+            });
+      } else {
+        student.name = _nameController.text;
+        student.course = _kelasController.text;
+
+        dbmanager.updateStudent(student).then((id) => {
+              setState(() {
+                studlist[updateIndex].name = _nameController.text;
+                studlist[updateIndex].course = _kelasController.text;
+              }),
+              _nameController.clear(),
+              _kelasController.clear(),
+              student = null
+            });
+      }
+    }
+  }
+}
